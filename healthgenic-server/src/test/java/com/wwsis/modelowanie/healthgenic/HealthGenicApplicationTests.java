d881341db5ecfbfe08a498571ddba96b44a02b21
jakbuowski1005 <jakubowski1005@gmail.com>
2020-11-22 16:16:55 +0100

    Create server app

---
+++ healthgenic-server/src/test/java/com/wwsis/modelowanie/healthgenic/HealthGenicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwsis.modelowanie.healthgenic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthGenicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
